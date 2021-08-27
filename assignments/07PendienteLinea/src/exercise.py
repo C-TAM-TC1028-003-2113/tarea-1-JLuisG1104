9d11dea8d215e77606444f7ca9f06de21d1c00e2
JLuisG1104 <A01411877@itesm.mx>
2021-08-27 11:53:08 -0500

    Terminé el ejercicio con comentarios.

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    # Aqui estoy pidiendo los datos iniciales
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    # Aqui calculo la pendiente que existe entre el punto 1 y punto 2
+    m = (y2 - y1) / (x2 - x1)
+    # Aqui muestro el resultado final al usuario
+    print("Pendiente:", m)
+
 
 
 if __name__ == '__main__':
