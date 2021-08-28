a63ee0cf8f9292f1f692b4b333dba74002228b72
JLuisG1104 <A01411877@itesm.mx>
2021-08-28 01:12:25 -0500

    Terminé el ejercicio del videojuego.

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -2,7 +2,6 @@ def main():
     # escribe tu código abajo de esta línea
     nuevo = int(input("Dame la cantidad de juegos nuevos: "))
     usado = int(input("Dame la cantidad de juegos usados: "))
-
     total = (nuevo * 1000) + (usado * 350)
 
     print("El total de la compra es:", total)
